999a30d6e4b98d000c298957adbecee1ae94bed7
Lívia Mendes <liviamendes089@gmail.com>
2022-11-30 19:47:04 -0300

    Add music player app

---
+++ js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name:"Run",
+        artist:"Joji",
+        img:"jojirun",
+        src:"jojirunmusic"
+     },
+    {
+        name:"Dance For You",
+        artist:"Beyoncé",
+        img:"beyonce",
+        src:"danceforyou"
+    },
+    {
+        name:"Candy",
+        artist:"Rosalía",
+        img:"rosalia",
+        src:"candy"
+    },
+    {
+        name:"No Hay Ley",
+        artist:"Kali Uchis",
+        img:"kaliuchis",
+        src:"nohayley"
+    },
+    {
+        name:"La Tortura",
+        artist:"Shakira",
+        img:"shakira",
+        src:"latortura"
+    },
+    {
+        name:"Oi, Sumido",
+        artist:"Nina",
+        img:"nina",
+        src:"oisumido"
+    },
+    {
+        name:"Ready or Not",
+        artist:"Fugees",
+        img:"fugees",
+        src:"readyornot"
+    },
+]
